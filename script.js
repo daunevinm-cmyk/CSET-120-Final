cf1c1c21c99071ed0b4c397a5204e543d3a84398
daunevinm-cmyk <daunevin06@gmail.com>
2025-11-07 10:50:12 -0500

    Daunevin, Menu Page, Ethan Sign Up Page, Joel Absent Co-authored-by: jaymiranda1411 <jaymiranda1411@users.noreply.github.com> Co-authored-by: Angrybones25 <Angrybones25@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,55 @@
+//Daunevin, 1-55, could change
+
+function glick1Macros() {
+    const button1 = document.getElementById("macros1")
+
+    button1.addEventListener("click", () => {
+            alert("Calories - 290 \n Protien - 11g \n Carbohydates - 24g \n Fat - 17g \n Saturated Fat - 7g \n Fiber - 1g \n Sugar - 3g \n Sodium - 850mg")
+
+    })
+}
+
+function glick2Macros() {
+    const button2 = document.getElementById("macros2")
+
+    button2.addEventListener("click", () => {
+            alert("Calories - 500 \n Protien - 20g \n Carbohydates - 35g \n Fat - 30g \n Saturated Fat - 11g \n Fiber - 2g \n Sugar - 6g \n Sodium - 1300mg")
+
+    })
+}
+
+function glick3Macros() {
+    const button3 = document.getElementById("macros3")
+
+    button3.addEventListener("click", () => {
+            alert("Calories - 400 \n Protien - 17g \n Carbohydates - 24g \n Fat - 25g \n Saturated Fat - 10g \n Fiber - 1g \n Sugar - 3g \n Sodium - 1000mg")
+
+    })
+}
+
+function glick4Macros() {
+    const button4 = document.getElementById("macros4")
+
+    button4.addEventListener("click", () => {
+            alert("Calories - 500 \n Protien - 19g \n Carbohydates - 34g \n Fat - 30g \n Saturated Fat - 11g \n Fiber - 2g \n Sugar - 4g \n Sodium - 1300mg")
+
+    })
+}
+
+function glick5Macros() {
+    const button5 = document.getElementById("macros5")
+
+    button5.addEventListener("click", () => {
+            alert("Calories - 460 \n Protien - 20g \n Carbohydates - 30g \n Fat - 30g \n Saturated Fat - 11g \n Fiber - 2g \n Sugar - 4g \n Sodium - 1100mg")
+
+    })
+}
+
+function glick6Macros() {
+    const button6 = document.getElementById("macros6")
+
+    button6.addEventListener("click", () => {
+            alert("Calories - 305 \n Protien - 11g \n Carbohydates - 30g \n Fat - 15g \n Saturated Fat - 7g \n Fiber - 1g \n Sugar - 3g \n Sodium - 850mg")
+
+    })
+}
